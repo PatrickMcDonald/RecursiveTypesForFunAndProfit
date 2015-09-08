5d58ca1c11fc95c3aacf804406b24a1c982e3363
Patrick McDonald <paddymcdonald@gmail.com>
2015-09-08 17:53:04 +0100

    Initial Commit

---
+++ RecursiveTypes/Part1.fsx
@@ -0,0 +1,142 @@
+﻿type Book = {title: string; price: decimal}
+
+type ChocolateType = Dark | Milk | SeventyPercent
+type Chocolate = {chocType: ChocolateType; price: decimal}
+
+type WrappingPaperStyle =
+    | HappyBirthday
+    | HappyHolidays
+    | SolidColor
+
+type Gift =
+    | Book of Book
+    | Chocolate of Chocolate
+    | Wrapped of Gift * WrappingPaperStyle
+    | Boxed of Gift
+    | WithACard of Gift * message:string
+
+let wolfHall = {title = "Wolf Hall"; price=20m}
+
+let yummyChoc = {chocType=Milk; price=5m}
+
+let birthdayPresent = WithACard (Wrapped (Book wolfHall, HappyBirthday), "Happy Birthday")
+
+let christmasPresent = Wrapped (Boxed (Chocolate yummyChoc), HappyHolidays)
+
+module ``A basic recursive type`` =
+    let rec description gift =
+        match gift with
+        | Book book ->
+            sprintf "'%s'" book.title
+        | Chocolate choc ->
+            sprintf "%A chocolate" choc.chocType
+        | Wrapped (innerGift,style) ->
+            sprintf "%s wrapped in %A paper" (description innerGift) style
+        | Boxed innerGift ->
+            sprintf "%s in a box" (description innerGift)
+        | WithACard (innerGift,message) ->
+            sprintf "%s with a card saying '%s'" (description innerGift) message
+
+    let rec totalCost gift =
+        match gift with
+        | Book book ->
+            book.price
+        | Chocolate choc ->
+            choc.price
+        | Wrapped (innerGift,style) ->
+            (totalCost innerGift) + 0.5m
+        | Boxed innerGift ->
+            (totalCost innerGift) + 1.0m
+        | WithACard (innerGift,message) ->
+            (totalCost innerGift) + 2.0m
+
+    let rec whatsInside gift =
+        match gift with
+        | Book book ->
+            "A book"
+        | Chocolate choc ->
+            "Some chocolate"
+        | Wrapped (innerGift,style) ->
+            whatsInside innerGift
+        | Boxed innerGift ->
+            whatsInside innerGift
+        | WithACard (innerGift,message) ->
+            whatsInside innerGift
+
+module ``A basic recursive type using Catamorphism`` =
+    let rec cataGift fBook fChocolate fWrapped fBoxed fCard gift : 'r =
+        let recurse = cataGift fBook fChocolate fWrapped fBoxed fCard
+        match gift with
+        | Book book -> fBook book
+        | Chocolate choc -> fChocolate choc
+        | Wrapped (gift,style) -> fWrapped (recurse gift,style)
+        | Boxed gift -> fBoxed (recurse gift)
+        | WithACard (gift,message) -> fCard (recurse gift,message)
+
+    let totalCost gift =
+        let fBook (book:Book) = book.price
+        let fChocolate (choc:Chocolate) = choc.price
+        let fWrapped (innerCost,style) = innerCost + 0.5m
+        let fBoxed innerCost = innerCost + 1.0m
+        let fCard (innerCost,message) = innerCost + 2.0m
+        cataGift fBook fChocolate fWrapped fBoxed fCard gift
+
+    let description gift =
+        let fBook (book:Book) = sprintf "'%s'" book.title
+        let fChocolate (choc:Chocolate) = sprintf "%A chocolate" choc.chocType
+        let fWrapped (innerText,style) = sprintf "%s wrapped in %A paper" innerText style
+        let fBoxed innerText = sprintf "%s in a box" innerText
+        let fCard (innerText,message) = sprintf "%s with a card saying '%s'" innerText message
+        cataGift fBook fChocolate fWrapped fBoxed fCard gift
+
+    let handleContents fBook fCocolate =
+        let fWrapped (gift,style) = gift
+        let fBoxed gift = gift
+        let fCard (gift,message) = gift
+        cataGift fBook fCocolate fWrapped fBoxed fCard
+
+    let whatsinside =
+        handleContents
+            (fun book -> "The book you wanted")
+            (fun choc -> "Your fave chocolate")
+
+let test f =
+    f birthdayPresent |> printfn "birthdayPresent: %A"
+    f christmasPresent |> printfn "christmasPresent: %A"
+
+;;
+
+test ``A basic recursive type``.description
+test ``A basic recursive type using Catamorphism``.description
+
+test ``A basic recursive type``.totalCost
+test ``A basic recursive type using Catamorphism``.totalCost
+
+test ``A basic recursive type``.whatsInside
+test ``A basic recursive type using Catamorphism``.whatsinside
+
+module Mapping =
+    type GiftMinusChocolate =
+        | Book of Book
+        | Apology of string
+        | Wrapped of GiftMinusChocolate * WrappingPaperStyle
+
+    let removeChocolate =
+        let fBook (book: Book) = Book book
+        let fChocolate (choc:Chocolate) = Apology "sorry I ate your chocolate"
+        let fWrapped (gift,style) = Wrapped (gift,style)
+        let fBoxed gift = gift
+        let fCard (gift,message) = gift
+        ``A basic recursive type using Catamorphism``.cataGift fBook fChocolate fWrapped fBoxed fCard
+
+test Mapping.removeChocolate
+
+let deepCopy = ``A basic recursive type using Catamorphism``.cataGift Book Chocolate Wrapped Boxed WithACard
+
+test deepCopy
+
+let upgradeChocolate =
+    let fChocolate (choc:Chocolate) = Chocolate {choc with chocType = SeventyPercent}
+    ``A basic recursive type using Catamorphism``.cataGift Book fChocolate Wrapped Boxed WithACard
+
+test upgradeChocolate
---
+++ RecursiveTypes/Part2.fsx
@@ -0,0 +1,145 @@
+﻿module FileSystem =
+
+    type FileSystemItem =
+        | File of File
+        | Directory of Directory
+    and File = {name:string; fileSize:int}
+    and Directory = {name:string; dirSize:int; subitems:FileSystemItem list}
+
+    let rec cataFS fFile fDir item : 'r =
+        let recurse = cataFS fFile fDir
+        match item with
+        | File file -> fFile file
+        | Directory dir ->
+            let listOfRs = dir.subitems |> List.map recurse
+            fDir (dir.name,dir.dirSize, listOfRs)
+
+    let totalSize fileSystemItem =
+        let fFile (file:File) = file.fileSize
+        let fDir (name,size,subsizes) = (List.sum subsizes) + size
+        cataFS fFile fDir fileSystemItem
+
+    let largestFile =
+        let ifNone deflt opt =
+            defaultArg opt deflt
+
+        let fileSize fileOpt =
+            fileOpt
+            |> Option.map (fun file -> file.fileSize)
+            |> ifNone 0
+
+        let fFile (file:File) = Some file
+
+        let fDir (name,size,subfiles) =
+            match subfiles with
+            | [] -> None
+            | subfiles -> subfiles |> List.maxBy fileSize
+
+        cataFS fFile fDir
+
+let readme = FileSystem.File {name="readme.txt"; fileSize=1}
+let config = FileSystem.File {name="config.xml"; fileSize=2}
+let build  = FileSystem.File {name="build.bat"; fileSize=3}
+let src = FileSystem.Directory {name="src"; dirSize=10; subitems=[readme; config; build]}
+let bin = FileSystem.Directory {name="bin"; dirSize=10; subitems=[]}
+let root = FileSystem.Directory {name="root"; dirSize=5; subitems=[src; bin]}
+
+readme |> FileSystem.totalSize
+src |> FileSystem.totalSize
+root |> FileSystem.totalSize
+
+readme |> FileSystem.largestFile
+src |> FileSystem.largestFile
+bin |> FileSystem.largestFile
+root |> FileSystem.largestFile
+
+module Products =
+    type Product =
+        | Bought of BoughtProduct
+        | Made of MadeProduct
+    and BoughtProduct = {
+        name: string
+        weight: int
+        vendor: string option }
+    and MadeProduct = {
+        name: string
+        weight: int
+        components: Component list }
+    and Component = {
+        qty: int
+        product: Product }
+
+    let rec cataProduct fBought fMade product : 'r =
+        let recurse = cataProduct fBought fMade
+
+        let convertComponentToTuple comp =
+            (comp.qty, recurse comp.product)
+
+        match product with
+        | Bought bought -> fBought bought
+        | Made made ->
+            let componentTuples =
+                made.components
+                |> List.map convertComponentToTuple
+            fMade (made.name,made.weight,componentTuples)
+
+    let productWeight =
+        let fBought (bought:BoughtProduct) = bought.weight
+        let fMade (name,weight,componentTuples) =
+            let componentWeight (qty,weight) = qty * weight
+            let totalComponentWeight =
+                componentTuples |> List.sumBy componentWeight
+            totalComponentWeight + weight
+        cataProduct fBought fMade
+
+    type VendorScore = {vendor:string; score:int}
+
+    let mostUsedVendor =
+        let fBought (bought:BoughtProduct) =
+            bought.vendor |> Option.map (fun vendor -> {vendor = vendor; score = 1})
+        let fMade (name,weight,subresults) =
+            let vendor vs = vs.vendor
+            let score vs = vs.score
+
+            let totalScore (vendor,vendorScores) =
+                let totalScore = vendorScores |> List.sumBy score
+                {vendor=vendor; score=totalScore}
+
+            subresults
+            |> List.choose snd
+            |> List.groupBy vendor
+            |> List.map totalScore
+            |> List.sortByDescending score
+            |> List.tryHead
+
+        cataProduct fBought fMade
+
+
+let label = Products.Bought {name = "label"; weight = 1; vendor = Some "ACME"}
+
+let bottle = Products.Bought {name = "bottle"; weight = 2; vendor = Some "ACME"}
+
+let formulation = Products.Bought {name = "formulation"; weight = 3; vendor = None}
+
+let shampoo =
+    Products.Made {name = "shampoo"; weight = 10; components =
+    [
+        {qty = 1; product = formulation}
+        {qty = 1; product = bottle}
+        {qty=2; product = label}
+    ]}
+
+let twoPack =
+    Products.Made {name = "twoPack"; weight = 5; components =
+    [
+        {qty = 2; product = shampoo}
+    ]}
+
+label |> Products.productWeight
+shampoo |> Products.productWeight
+twoPack |> Products.productWeight
+
+label |> Products.mostUsedVendor
+formulation |> Products.mostUsedVendor
+shampoo |> Products.mostUsedVendor
+twoPack |> Products.mostUsedVendor
