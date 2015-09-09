f5fcd14dcb27fcee43789b6570c607954d0af33b
Patrick McDonald <paddymcdonald@gmail.com>
2015-09-09 17:57:55 +0100

    Parts 3 & 4

---
+++ RecursiveTypes/Part3.fsx
@@ -0,0 +1,183 @@
+﻿type Book = {
+    title: string
+    price: decimal
+    }
+
+type ChocolateType =
+    | Dark
+    | Milk
+    | SeventyPercent
+
+type Chocolate = {
+    chocType: ChocolateType
+    price: decimal
+    }
+
+type WrappingPaperStyle =
+    | HappyBirthday
+    | HappyHolidays
+    | SolidColor
+
+type Gift =
+    | Book of Book
+    | Chocolate of Chocolate
+    | Wrapped of Gift * WrappingPaperStyle
+    | Boxed of Gift
+    | WithACard of Gift * message:string
+
+// A Book
+let wolfHall = {title="Wolf Hall"; price=20m}
+// A Chocolate
+let yummyChoc = {chocType=SeventyPercent; price=5m}
+// A Gift
+let birthdayPresent = WithACard (Wrapped (Book wolfHall, HappyBirthday), "Happy Birthday")
+// A Gift
+let christmasPresent = Wrapped (Boxed (Chocolate yummyChoc), HappyHolidays)
+
+let rec cataGift fBook fChocolate fWrapped fBox fCard gift :'r =
+    let recurse = cataGift fBook fChocolate fWrapped fBox fCard
+    match gift with
+    | Book book -> fBook book
+    | Chocolate choc -> fChocolate choc
+    | Wrapped (gift,style) -> fWrapped (recurse gift) style
+    | Boxed gift -> fBox (recurse gift)
+    | WithACard (gift,message) -> fCard (recurse gift) message
+
+let totalCostUsingCata =
+    let fBook (book:Book) =
+        book.price
+    let fChocolate (choc:Chocolate) =
+        choc.price
+    let fWrapped innerCost style =
+        innerCost + 0.5m
+    let fBox innerCost =
+        innerCost + 1.0m
+    let fCard innerCost message =
+        innerCost + 2.0m
+    // call the catamorphism
+    cataGift fBook fChocolate fWrapped fBox fCard
+
+let deeplyNestedBox depth =
+    let rec loop depth boxSoFar =
+        match depth with
+        | 0 -> boxSoFar
+        | n -> loop (n-1) (Boxed boxSoFar)
+    loop depth (Book wolfHall)
+
+deeplyNestedBox 5
+
+deeplyNestedBox 10
+
+deeplyNestedBox 10 |> totalCostUsingCata
+deeplyNestedBox 100 |> totalCostUsingCata
+deeplyNestedBox 1000 |> totalCostUsingCata
+
+[10; 100; 1000]
+|> List.map (fun depth -> deeplyNestedBox depth |> totalCostUsingCata)
+
+// stackoverflow deeplyNestedBox 10000 |> totalCostUsingCata
+// stackoverflow deeplyNestedBox 100000 |> totalCostUsingCata
+
+let totalCostUsingAcc =
+    let rec loop costSoFar gift =
+        match gift with
+        | Book book ->
+            costSoFar + book.price
+        | Chocolate choc ->
+            costSoFar + choc.price
+        | Wrapped (gift,style) ->
+            loop (costSoFar + 0.5m) gift
+        | Boxed gift ->
+            loop (costSoFar + 1.0m) gift
+        | WithACard (gift,message) ->
+            loop (costSoFar + 2.0m) gift
+    loop 0.0m
+
+[10; 100; 1000; 10000; 100000; 1000000]
+|> List.map (fun depth -> deeplyNestedBox depth |> totalCostUsingAcc)
+
+let rec foldGift fBook fChocolate fWrapped fBox fCard acc gift : 'r =
+    let recurse = foldGift fBook fChocolate fWrapped fBox fCard
+    match gift with
+    | Book book ->
+        fBook acc book
+    | Chocolate choc ->
+        fChocolate acc choc
+    | Wrapped (gift,style) ->
+        let newAcc = fWrapped acc style
+        recurse newAcc gift
+    | Boxed gift ->
+        let newAcc = fBox acc
+        recurse newAcc gift
+    | WithACard (gift,message) ->
+        let newAcc = fCard acc message
+        recurse newAcc gift
+
+let totalCostUsingFold =
+    let fBook acc (book:Book) = acc + book.price
+    let fChocolate acc (choc:Chocolate) = acc + choc.price
+    let fWrapped acc style = acc + 0.5m
+    let fBox acc = acc + 1.0m
+    let fCard acc message = acc + 2.0m
+    foldGift fBook fChocolate fWrapped fBox fCard 0.0m
+
+[10; 100; 1000; 10000; 100000; 1000000]
+|> List.map (fun depth -> deeplyNestedBox depth |> totalCostUsingFold)
+
+let descriptionUsingFold =
+    let fBook acc (book:Book) = sprintf "'%s' %s" book.title acc
+    let fChocolate acc (choc:Chocolate) = sprintf "%A chocolate %s" choc.chocType acc
+    let fWrapped acc style = sprintf "%s wrapped in %A paper" acc style
+    let fBox acc = sprintf "%s in a box" acc
+    let fCard acc message = sprintf "%s with a card saying '%s'" acc message
+    foldGift fBook fChocolate fWrapped fBox fCard ""
+
+birthdayPresent |> descriptionUsingFold
+christmasPresent |> descriptionUsingFold
+
+let descriptionUsingFoldWithGenerator =
+    let fBook gen (book:Book) =
+        gen (sprintf "'%s'" book.title)
+    let fChocolate gen (choc:Chocolate) =
+        gen (sprintf "%A chocolate" choc.chocType)
+    let fWrapped gen style innerText =
+        gen (sprintf "%s wrapped in %A paper" innerText style)
+    let fBox gen innerText =
+        gen (sprintf "%s in a box" innerText)
+    let fCard gen message innerText =
+        gen (sprintf "%s with a card saying '%s'" innerText message)
+    foldGift fBook fChocolate fWrapped fBox fCard id
+
+birthdayPresent |> descriptionUsingFoldWithGenerator
+christmasPresent |> descriptionUsingFoldWithGenerator
+
+let rec foldBackGift fBook fChocolate fWrapped fBox fCard gift gen : 'r =
+    let recurse = foldBackGift fBook fChocolate fWrapped fBox fCard
+    match gift with
+    | Book book ->
+        gen (fBook book)
+    | Chocolate choc ->
+        gen (fChocolate choc)
+    | Wrapped (gift,style) ->
+        recurse gift (fun innerVal -> gen (fWrapped style innerVal))
+    | Boxed gift ->
+        recurse gift (fun innerVal -> gen (fBox innerVal))
+    | WithACard (gift,message) ->
+        recurse gift (fun innerVal -> gen (fCard message innerVal))
+
+let descriptionUsingFoldBack gift =
+    let fBook (book:Book) =
+        sprintf "'%s'" book.title
+    let fChocolate (choc:Chocolate) =
+        sprintf "%A chocolate" choc.chocType
+    let fWrapped style innerText =
+        sprintf "%s wrapped in %A paper" innerText style
+    let fBox innerText =
+        sprintf "%s in a box" innerText
+    let fCard message innerText =
+        sprintf "%s with a card saying '%s'" innerText message
+    foldBackGift fBook fChocolate fWrapped fBox fCard gift id
+
+birthdayPresent |> descriptionUsingFoldBack
+christmasPresent |> descriptionUsingFoldBack
+
---
+++ RecursiveTypes/Part4.fsx
@@ -0,0 +1,54 @@
+﻿type FileSystemItem =
+    | File of File
+    | Directory of Directory
+and File = {
+    name:string
+    fileSize:int
+    }
+and Directory = {
+    name:string
+    dirSize:int
+    subitems:FileSystemItem list
+    }
+
+let readme = File {name="readme.txt"; fileSize=1}
+let config = File {name="config.xml"; fileSize=2}
+let build  = File {name="build.bat"; fileSize=3}
+let src = Directory {name="src"; dirSize=10; subitems=[readme; config; build]}
+let bin = Directory {name="bin"; dirSize=10; subitems=[]}
+let root = Directory {name="root"; dirSize=5; subitems=[src; bin]}
+
+let fileSystemItems = [readme; config; build; src; bin; root]
+
+let rec foldFS fFile fDir acc item : 'r =
+    let recurse = foldFS fFile fDir
+    match item with
+    | File file ->
+        fFile acc file
+    | Directory dir ->
+        let newAcc = fDir acc (dir.name, dir.dirSize)
+        dir.subitems |> List.fold recurse newAcc
+
+let totalSize =
+    let fFile acc (file:File) = acc + file.fileSize
+    let fDir acc (name,size) = acc + size
+    foldFS fFile fDir 0
+
+fileSystemItems |> List.map totalSize
+
+
+let largestItem =
+    let fFile (largestSoFarOpt:File option) (file:File) =
+        match largestSoFarOpt with
+        | None -> Some file
+        | Some largestSoFar ->
+            if largestSoFar.fileSize > file.fileSize then
+                Some largestSoFar
+            else
+                Some file
+    let fDir largestSoFarOpt (name,size) = largestSoFarOpt
+
+    foldFS fFile fDir None
+
+fileSystemItems |> List.map largestItem
+
